fcd547d56e1b20e37c9bc98ff736cf9477f9d644
Tarak-Chandra-Sarkar <tarak.chandra.sarkar@hotmail.com>
2018-12-27 15:57:10 +0530

    Creating Route Constraint

--- UnderstandingRoutingInAspNetMvc/App_Start/RouteConfig.cs
+++ UnderstandingRoutingInAspNetMvc/App_Start/RouteConfig.cs
@@ -10,9 +10,16 @@ namespace UnderstandingRoutingInAspNetMvc
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Product",
+                url: "Product/{productId}",
+                defaults: new { controller = "Product", action = "Details" },
+                constraints: new { productId = @"\d+" }
+            );
+
+            routes.MapRoute(
                 name: "Blog",                               // Route name
                 url: "Archive/{entryDate}",                 // URL with parameters
-                defaults: new {controller= "Archive", action="Entry"}  // Parameter defaults
+                defaults: new { controller = "Archive", action = "Entry" }  // Parameter defaults
             );
 
             routes.MapRoute(
@@ -20,6 +27,7 @@ namespace UnderstandingRoutingInAspNetMvc
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
---
+++ UnderstandingRoutingInAspNetMvc/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace UnderstandingRoutingInAspNetMvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Details(int productId)
+        {
+            ViewBag.ProductDetails = $"ProductId: {productId}";
+            return View();
+        }
+    }
+}
